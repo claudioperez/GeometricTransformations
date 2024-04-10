fd2cbcf30f25b0c65e9b5c9120531f4685967bb7
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-04-10 11:11:55 -0700

    cmp

--- Rotations/Rotations.hpp
+++ Rotations/Rotations.hpp
@@ -1,7 +1,13 @@
 #include <math.h>
 #include "Matrix3D.h"
 #include "Vector3D.h"
+#include "Quaternion.h"
 using OpenSees::Matrix3D;
+typedef ASDQuaternion<double> Versor;
+
+Vector3D Versor2Vector(Versor);
+Versor   Matrix2Versor(Matrix3D);
+// matrix_to_versor
 
 static constexpr Matrix3D Eye3 {{{
   {1, 0, 0},
@@ -36,14 +42,103 @@ Matrix3D Spin(const Vector3D &u)
 // refactored by Claudio M. Perez                                                       2023
 // -----------------------------------------------------------------------------------------
 
-  return Matrix3D {{{  0  ,  u(3), -u(2)},
+  return Matrix3D{{{{  0  ,  u(3), -u(2)},
                     {-u(3),   0  ,  u(1)},
-                    { u(2), -u(1),   0  }}};
+                    { u(2), -u(1),   0  }}}};
+}
+
+void GibSO3(const Vector3D &vec, double *a, double *b=nullptr, double *c=nullptr)
+{
+//
+//Compute coefficients of the Rodrigues formula and their derivatives.
+//
+//                       [1,2]     | [3]
+//                       ----------+-------
+//                       a1        |
+//                       a2        | c4
+//                       a3        | c5
+//
+//                       b1 = -c0  | c1
+//                       b2        | c2 
+//                       b3        | c3
+//
+//
+// [1] Perez, C. M., and Filippou F. C. (2024) "On Nonlinear Geometric 
+//     Transformations of Finite Elements" 
+//     Int. J. Numer. Meth. Engrg. 2024 (Expected)
+//
+// [2] Ritto-Corrêa, M. and Camotim, D. (2002) "On the differentiation of the
+//     Rodrigues formula and its significance for the vector-like parameterization
+//     of Reissner-Simo beam theory"
+//     Int. J. Numer. Meth. Engrg., 55(9), pp.
+//     1005–1032. Available at: https://doi.org/10.1002/nme.532.
+//
+// [3] Ibrahimbegović, A. and Mikdad, M.A. (1998) ‘Finite rotations in dynamics of
+//     beams and implicit time‐stepping’, 41, pp. 781–814.
+//
+// [4] Pfister, F. (1998) ‘Bernoulli Numbers and Rotational Kinematics’,
+//     Journal of Applied Mechanics, 65(3), pp. 758–763. 
+//     Available at: https://doi.org/10.1115/1.2789120.
+//
+// =========================================================================================
+// function by Claudio Perez                                                            2023
+// -----------------------------------------------------------------------------------------
+//
+  double angle2 =  vec.dot(vec);
+
+  if (angle2  <= 1e-07) {
+    if (a != nullptr) {
+      a[0]   =   0.0;
+      a[1]   =   1.0        - angle2*(1.0/6.0   - angle2*(1.0/120.0  - angle2/5040.0));
+      a[2]   =   0.5        - angle2*(1.0/24.0  - angle2*(1.0/720.0  - angle2/40320.0));
+      a[3]   =   1.0/6.0    - angle2/(1.0/120.0 - angle2/(1.0/5040.0 - angle2/362880.0));
+    }
+    if (b != nullptr) {
+      b[1]   = - 1.0/3.0    + angle2*(1.0/30.0  - angle2*(1.0/840.0  - angle2/45360.0));
+      b[2]   = - 1.0/12.0   + angle2*(1.0/180.0 - angle2*(1.0/6720.0 - angle2/453600.0));
+      b[3]   = - 1.0/60.0   + angle2*(1.0/1260.0 - angle2*(1.0/60480 - angle2/4989600.0));
+    }
+    if (c != nullptr) {
+      c[1]   = b[3] - b[2]; 
+      c[2]   = 1.0/90.0     - angle2*(1.0/1680.0 - angle2*(1.0/75600.0 - angle2/5987520.0)); 
+      c[3]   = 1.0/630.0    - angle2*(1.0/15120.0 - angle2*(1.0/831600.0 - angle2/77837760.0));
+    }
+
+  } else {
+
+    double angle  = vec.norm();
+//  double angle  = sqrt(angle2);
+    double sn     = sin(angle);
+    double cs     = cos(angle);
+    double angle3 = angle*angle2;
+    double angle4 = angle*angle3;
+    double angle5 = angle*angle4;
+    double angle6 = angle*angle5;
+                                                    
+    if (a != nullptr) {
+      a[0] = cs;
+      a[1] = sn / angle;   
+      a[2] = ( 1.0 - a[0] ) / angle2;    
+  //  a[3] = ( 1.0 - a[1] ) / angle2;
+      a[3] = (angle - sn)/(angle3);
+    }
+
+    if (b != nullptr) {
+      b[1] = ( angle*cs - sn)/angle3;  
+      b[2] = ( angle*sn - 2 + 2*cs)/angle4;    
+      b[3] = ( 3*sn - 2*angle -  angle*cs )/angle5;    
+    }
+    if (c != nullptr) {
+      c[1] = (3*sn - angle2*sn - 3*angle*cs)/(angle5);
+      c[2] = (8 - 8*cs - 5*angle*sn + angle2*cs)/(angle5*angle);
+      c[3] = (8*angle + 7*angle*cs + angle2*sn - 15*sn)/(angle5*angle2);
+    }
+  }
 }
 
 
 
-Matrix3D ddTanSO3(const Vector3D &theta, const Vector3D &a, const Vector3D &b)
+Matrix3D ddTanSO3(const Vector3D &v, const Vector3D &p, const Vector3D &q)
 {
 // [1] Perez, C.M., and Filippou F. C.. "On Nonlinear Geometric 
 //     Transformations of Finite Elements" 
@@ -52,21 +147,43 @@ Matrix3D ddTanSO3(const Vector3D &theta, const Vector3D &a, const Vector3D &b)
 // =========================================================================================
 // function by Claudio Perez                                                            2023
 // -----------------------------------------------------------------------------------------
-  double a0, a1, a2, a3, b1, b2, b3, c1, c2, c3;
-  GibSO3(theta);
+  double a[4], b[4], c[4];
+  GibSO3(v, a, b, c);
 
-  const Vector3D axb = a.cross(b);
-  const Vector3D txa = theta.cross(a);
+  const Vector3D pxq = p.cross(q);
+  const Vector3D vxp = v.cross(p);
 
-  return a3*(a.bun(b) + b.bun(a)) + b1*a.dot(b)*Eye3
-       + b2*(axb.bun(theta) + theta.bun(axb) + txa.dot(b)*Eye3)
-       + b3*( theta.dot(a)*(b.bun(theta) + theta.bun(b)) 
-            + theta.dot(b)*(a.bun(theta) + theta.bun(a)) 
-            + theta.dot(a)*theta.dot(b)*Eye3) 
-       + (c1*a.dot(b) + c2*(txa.dot(b)) + c3*theta.dot(a)*theta.dot(b))*theta.bun(theta);
+  const Matrix3D psq { p.bun(q) + q.bun(p) };
+  const Matrix3D vov { v.bun(v) };
+
+  Matrix3D dT{0.0};
+
+  dT.addTensorProduct(p, q, a[3])
+    .addTensorProduct(q, p, a[3])
+    .addDiagonal(b[1]*p.dot(q))
+    .addTensorProduct(pxq, v, b[2])
+    .addTensorProduct(v, pxq, b[2])
+    .addDiagonal(vxp.dot(q)*b[2])
+
+    .addTensorProduct(q, v, b[3]*v.dot(p))
+    .addTensorProduct(v, q, b[3]*v.dot(p))
+    .addTensorProduct(p, v, b[3]*v.dot(q))
+    .addTensorProduct(v, p, b[3]*v.dot(q))
+    .addDiagonal(v.dot(p)*v.dot(q)*b[3])
+
+    .addTensorProduct(v, v, c[1]*p.dot(q) + c[2]*(vxp.dot(q)) + c[3]*v.dot(p)*v.dot(q));
+
+  return dT;
+
+//return a[3]*psq + b[1]*p.dot(q)*Eye3
+//     + b[2]*(pxq.bun(v) + v.bun(pxq) + vxp.dot(q)*Eye3)
+//     + b[3]*( v.dot(p)*(q.bun(v) + v.bun(q)) 
+//          + v.dot(q)*(p.bun(v) + v.bun(p)) 
+//          + v.dot(p)*v.dot(q)*Eye3) 
+//     + vov*(c[1]*p.dot(q) + c[2]*(vxp.dot(q)) + c[3]*v.dot(p)*v.dot(q));
 }
 
-Matrix3D dExpSO3(const Vector3D &th, const Vector3D &dth)
+Matrix3D dExpSO3(const Vector3D &v)
 {
 // [1] Perez, C.M., and Filippou F. C.. "On Nonlinear Geometric 
 //     Transformations of Finite Elements" 
@@ -76,12 +193,18 @@ Matrix3D dExpSO3(const Vector3D &th, const Vector3D &dth)
 // function by Claudio Perez                                                            2023
 // -----------------------------------------------------------------------------------------
 
-  //Form first Gib coefficients
-  [~,a1, a2, a3] = GibSO3(th);
-  //Form skew-symmetric matrix from 3-vector th
-  Matrix3D Th = Spin(th);
+  // Form first Gib coefficients
+  double a[4];
+  GibSO3(v, a);
 
-  return a1*Eye3 + a2*Th + a3*th.bun(th);
+  Matrix3D T{0.0};
+  T.addDiagonal(a[1])
+   .addSpin(v, a[2])
+   .addTensorProduct(v, v, a[3]);
+
+  return T;
+
+//return a[1]*Eye3 + a[2]*Th + a[3]*v.bun(v);
 }
 
 Matrix3D dExpInvSO3(const Vector3D &v)
@@ -157,7 +280,7 @@ Matrix3D ddExpInvSO3(const Vector3D& th, const Vector3D& v)
   return dH*dLogSO3(th);
 }
 
-Matrix3D dTanSO3(const Vector3D &th, const Vector3D &a, char repr)
+Matrix3D dTanSO3(const Vector3D &v, const Vector3D &p, char repr)
 {
 //
 // repr     'L' or 'R' indicating left or right representation, 
@@ -170,121 +293,41 @@ Matrix3D dTanSO3(const Vector3D &th, const Vector3D &a, char repr)
     repr = 'L';
   }
 
+  double a[4], b[4];
+  GibSO3(v, a, b);
 
-  [~, a1, a2, a3, b1, b2, b3] = GibSO3(th);
-
-  Matrix3D txaot = th.cross(a).bun(th);
+  Matrix3D vxpov = v.cross(p).bun(v);
 
   Matrix3D Xi;
   switch (repr) {
     case 'R':
-      Xi = - a2*Spin(a) + a3*th.dot(a)*Eye3 + a3*th.bun(a)
-           + b1*a.bun(th) + b2*txaot + b3*th.dot(a)*th.bun(th);
+      Xi = - a2*Spin(p) + a3*v.dot(p)*Eye3 + a3*v.bun(p)
+           + b1*a.bun(v) + b2*vxpov + b3*v.dot(p)*v.bun(v);
     case 'L':
-      Xi =   a2*Spin(a) + a3*th.dot(a)*Eye3 + a3*th.bun(a)
-           + b1*a.bun(th) - b2*txaot + b3*th.dot(a)*th.bun(th);
+      Xi =   a2*Spin(p) + a3*v.dot(p)*Eye3 + a3*v.bun(p)
+           + b1*a.bun(v) - b2*vxpov + b3*v.dot(p)*v.bun(v);
   }
   return Xi;
 }
 
-Matrix3D ExpSO3(const Vector3D &th, const Vector3D &other)
+Matrix3D ExpSO3(const Vector3D &v)
 {
 // =========================================================================================
 // function by Claudio Perez                                                            2023
 // -----------------------------------------------------------------------------------------
 
   //Form the first Gib coefficients
-  [a0, a1, a2] = GibSO3(th);
+  double a[4];
+  GibSO3(th, a);
 
   //Form 3x3 skew-symmetric matrix Th from axial vector th
-  Matrix3D Th = Spin(th);
+  Matrix3D Th = Spin(v);
 
-  return Eye3 + a1*Th + a2*Th*Th;
+  return Eye3 + a[1]*Th + a[2]*Th*Th;
 
 }
 
 
-void GibSO3(const Vector3D &vec)
-{
-//
-//Compute coefficients of the Rodrigues formula and their derivatives.
-//
-//                       [1,2]     | [3]
-//                       ----------+-------
-//                       a1        |
-//                       a2        | c4
-//                       a3        | c5
-//
-//                       b1 = -c0  | c1
-//                       b2        | c2 
-//                       b3        | c3
-//
-//
-// [1] Perez, C. M., and Filippou F. C. (2024) "On Nonlinear Geometric 
-//     Transformations of Finite Elements" 
-//     Int. J. Numer. Meth. Engrg. 2024 (Expected)
-//
-// [2] Ritto-Corrêa, M. and Camotim, D. (2002) "On the differentiation of the
-//     Rodrigues formula and its significance for the vector-like parameterization
-//     of Reissner-Simo beam theory"
-//     Int. J. Numer. Meth. Engrg., 55(9), pp.
-//     1005–1032. Available at: https://doi.org/10.1002/nme.532.
-//
-// [3] Ibrahimbegović, A. and Mikdad, M.A. (1998) ‘Finite rotations in dynamics of
-//     beams and implicit time‐stepping’, 41, pp. 781–814.
-//
-// [4] Pfister, F. (1998) ‘Bernoulli Numbers and Rotational Kinematics’,
-//     Journal of Applied Mechanics, 65(3), pp. 758–763. 
-//     Available at: https://doi.org/10.1115/1.2789120.
-//
-// =========================================================================================
-// function by Claudio Perez                                                            2023
-// -----------------------------------------------------------------------------------------
-//
-  double angle2 =  vec.dot(vec);
-  double a0, a1, a2, a3, b1, b2, b3, c1, c2, c3;
-
-  if (angle2  <= 1e-07) {
-
-    a0    =   0.0;
-    a1    =   1.0        - angle2*(1.0/6.0   - angle2*(1.0/120.0  - angle2/5040.0));
-    a2    =   0.5        - angle2*(1.0/24.0  - angle2*(1.0/720.0  - angle2/40320.0));
-    a3    =   1.0/6.0    - angle2/(1.0/120.0 - angle2/(1.0/5040.0 - angle2/362880.0));
-    b1    = - 1.0/3.0    + angle2*(1.0/30.0  - angle2*(1.0/840.0  - angle2/45360.0));
-    b2    = - 1.0/12.0   + angle2*(1.0/180.0 - angle2*(1.0/6720.0 - angle2/453600.0));
-    b3    = - 1.0/60.0   + angle2*(1.0/1260.0 - angle2*(1.0/60480 - angle2/4989600.0));
-    c1    = b3   - b2; 
-    c2    = 1.0/90.0     - angle2*(1.0/1680.0 - angle2*(1.0/75600.0 - angle2/5987520.0)); 
-    c3    = 1.0/630.0    - angle2*(1.0/15120.0 - angle2*(1.0/831600.0 - angle2/77837760.0));
-
-  } else {
-
-    double angle  = vec.norm();
-//  double angle  = sqrt(angle2);
-    double sn     = sin(angle);
-    double cs     = cos(angle);
-    double angle3 = angle*angle2;
-    double angle4 = angle*angle3;
-    double angle5 = angle*angle4;
-    double angle6 = angle*angle5;
-                                                    
-    a0   = cs;
-    a1   = sn / angle;   
-    a2   = ( 1.0 - a0 ) / angle2;    
-//  a3   = ( 1.0 - a1 ) / angle2;
-    a3   = (angle - sn)/(angle3);
-
-    if (nargout > 4) {
-      b1   = ( angle*cs - sn)/angle3;  
-      b2   = ( angle*sn - 2 + 2*cs)/angle4;    
-      b3   = ( 3*sn - 2*angle -  angle*cs )/angle5;    
-
-      c1 = (3*sn - angle2*sn - 3*angle*cs)/(angle5);
-      c2 = (8 - 8*cs - 5*angle*sn + angle2*cs)/(angle5*angle);
-      c3 = (8*angle + 7*angle*cs + angle2*sn - 15*sn)/(angle5*angle2);
-    }
-  }
-}
 
 Vector3D LogSO3(const Matrix3D &R)
 {
@@ -321,7 +364,7 @@ Vector3D LogSO3(const Matrix3D &R)
 // function by Claudio Perez                                                            2023
 // -----------------------------------------------------------------------------------------
 
-  return Quat2Axis(Rmat2Quat(R));
+  return Versor2Vector(Matrix2Versor(R));
 
 }
 
