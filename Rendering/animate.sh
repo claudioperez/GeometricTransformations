a5036da4c8d394b75957f5ecc6d01a0afcc9e4b5
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-04-14 07:46:29 -0700

    cmp - clean up

--- Rendering/animate.sh
+++ Rendering/animate.sh
@@ -1,3 +1,4 @@
 input="$1"
 shift
-python -m veux.motion $input $input --style frame.surface.scale=10 --recover iter --show origin.axes $@
+python -m veux.motion $input $input --style frame.surface.scale=10 \
+       --recover-rotations iter --show origin.axes $@
